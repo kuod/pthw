730e0798a1943e4d5c6f2b294d6309cc0b22c54e
David Kuo <dkuo@cbio.mskcc.org>
2013-11-22 02:51:31 -0500

    first three exercises

---
+++ ex1.py
@@ -0,0 +1,31 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+#Study Exercises
+#1. Make your script print another line.
+#2. Make your script print only one of the lines.
+#3. Put a '#' (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+print "##########################"
+print "1: Make your script print another line."
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "another line!"
+
+print "##########################"
+#2. Make your script print only one of the lines.
+print "2. Make your script print only one of the lines."
+if True:
+	print "only one line"
+print "##########################"
+print "3. Put a '#' (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does."
+print 'No lines printed; all commented out'
---
+++ ex3.py
@@ -0,0 +1,29 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+#evaluates
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#evaluates 5 < -2
+print 3 + 2 < 5 - 7
+
+#prints sum
+print "What is 3 + 2?", 3 + 2
+
+#prints sum
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+#evaluates 5 greater than -2
+print "Is it greater?", 5 > -2
+#evaluates if 5 is greater than -2
+print "Is it greater or equal?", 5 >= -2
+#Evaluate if 5 is less than or equal to -2
+print "Is it less or equal?", 5 <=-2
