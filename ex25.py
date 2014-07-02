4b027751f848b646d7219403cb54fcb61a446b9c
David Kuo <dkuo@cbio.mskcc.org>
2014-07-02 01:24:37 -0400

    updating to ex35

--- ex25.py
+++ ex25.py
@@ -22,7 +22,7 @@ def sort_sentence(sentence):
 	words = break_words(sentence)
 	return sort_words(words)
 
-def print_first_and_last(sentence)
+def print_first_and_last(sentence):
 	"""Prints the first and last words of the sentence."""
 	words = break_words(sentence)
 	print_first_word(words)
@@ -32,6 +32,4 @@ def print_first_and_last_sorted(sentence):
 	"""Sorts the words then prints the first and last one."""
 	words = sort_sentence(sentence)
 	print_first_word(words)
-	print_last_word(words
-
-	 
+	print_last_word(words)
